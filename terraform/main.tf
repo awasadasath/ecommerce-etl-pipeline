daeedf84fbe5d2ea7825d90b1047e60ba586bfb6
awasadasath <awasada.sath@gmail.com>
2026-01-13 23:50:36 +0700

    logic fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,6 @@ resource "google_storage_bucket" "data_lake" {
   location      = "US"
   storage_class = "STANDARD"
   force_destroy = true
-  uniform_bucket_level_access = true
 }
 
 resource "google_bigquery_dataset" "dataset" {
